5906eec9dbb44b8926da6956e3ca9e869781b963
YanjieMa <18722067752@163.com>
2023-04-12 10:19:57 +0800

    20230412

---
+++ 2_OS/osdev/includes/osdev/arch/x86/idt.h
@@ -0,0 +1,18 @@
+#ifndef __IDT_H__
+#define __IDT_H__
+
+/*
+The GDT is pointed to by the value in the GDTR register. This is loaded using the LGDT assembly instruction, whose argument is a pointer to a GDT Descriptor structure:
+
+
+
+
+
+ */
+
+
+
+
+
+
+#endif
